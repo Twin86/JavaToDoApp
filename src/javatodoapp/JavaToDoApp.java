98a00e0d43b5f846c043aba2201f731a0ab3b226
Twin86 <twin86@gmail.com>
2016-11-07 23:27:34 +0100

    Add some class

--- src/javatodoapp/JavaToDoApp.java
+++ src/javatodoapp/JavaToDoApp.java
@@ -4,6 +4,9 @@
  * and open the template in the editor.
  */
 package javatodoapp;
+import user.User;
+import database.DataBase;
+
 
 /**
  *
@@ -15,7 +18,11 @@ public class JavaToDoApp {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // TODO code
+        User u = new User("Sebastian");
+        DataBase db = new DataBase();
+        System.out.print(u.getSelfInfo());
+        System.out.print(db.getSelf());
     }
     
 }
